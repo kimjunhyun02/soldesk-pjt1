46385c600e6cd2940376a1dd4601aa4adfa6ac05
kimjunhyun02 <junkimh0904@gmail.com>
2025-11-10 15:14:57 +0900

    Create nfs-server.sh

---
+++ nfs-server.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+apt install -y nfs-kernel-server
+mkdir /shared
+chmod 777 /shared
+echo "/shared 172.16.*(rw,sync)" > /etc/exports
+systemctl start nfs-server
+ec2metadata -o | cut -d ' ' -f 2 >> /shared/list.txt
+chmod 777 /shared/list.txt
